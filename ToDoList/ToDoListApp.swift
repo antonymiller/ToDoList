da6a9eb0afb5769294902dcbff85f4d464d40679
antonymiller <54910473+antonymiller@users.noreply.github.com>
2023-07-26 14:24:18 -0500

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by antony miller on 7/26/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
